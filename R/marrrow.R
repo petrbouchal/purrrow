9a06f9ded5a1d2c1477eb7e9a2c923316decf595
Petr Bouchal <pbouchal@gmail.com>
2021-01-26 23:36:21 +0100

    rename, export and document first core function

--- R/marrrow.R
+++ R/marrrow.R
@@ -1,4 +1,28 @@
-marrow_dir <- function(.x, .f, ..., .path, .partitioning = c(), .format = "parquet") {
+
+#' Iteratively collate output of function into an Arrow dataset out of memory
+#'
+#'`r lifecycle::badge('experimental')`
+#' map + arrow: iterate over a function and collate the results into
+#'   an Arrow dataset. This happens without the whole dataset being in memory,
+#'   so is suitable for large data objects. The function must return a data.frame or
+#'   tibble. The returned value is a path to the directory containing the
+#'   Arrow dataset.
+#'
+#' @param .x vector or list of values for .f to iterate over
+#' @param .f function; must return a data.frame/tibble
+#' @param ... other arguments to .f
+#' @param .path path to directory where collated Arrow dataset will be stored.
+#'   will be created if it does not exist
+#' @param .partitioning character vector of columns to use for partitioning.
+#'   Columns must exist in output of .f.
+#' @param .format "parquet" (the default) or "arrow".
+#'
+#' @return path to new dataset directory; character string of length one.
+#' @export
+#' @examples
+#' # ADD_EXAMPLES_HERE
+marrow_dir <- function(.x, .f, ..., .path, .partitioning = c(),
+                       .format = "parquet") {
   arrow_temp_dirs <- file.path(tempdir(),
                                random_string(n = length(.x), length = 30))
 
